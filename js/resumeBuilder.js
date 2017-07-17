3261c8ce598b6bb32bac2d105ba6937cddd8a7ba
blinklv <blinklv@icloud.com>
2017-07-17 21:06:16 +0800

    Cleanup  and save four objects.

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -3,9 +3,65 @@
 // Author: blinklv <blinklv@icloud.com>
 // Create Time: 2017-07-14
 // Maintainer: blinklv <blinklv@icloud.com>
-// Last Change: 2017-07-14
+// Last Change: 2017-07-17
 
 (function() {
-  $("#main").append("blinklv");
+  var bio = {
+    name: "Blink Lv",
+    role: "Web Developer",
+    contacts: {
+      mobile: "650-555-5555",
+      email: "blinklv@icloud.com",
+      github: "blinklv",
+      twitter: "@blinklv",
+      location: "ShenZhen"
+    },
+    welcomeMessage: "Who Are You?",
+    skills: ["programming", "mathsmatics", "painting"],
+    bioPic: "images/fry.jpg"
+  };
+
+  var education = {
+    schools: [
+    { 
+      name: "Xi'an University of technology",
+      city: "Xi'an",
+      degree: "Masters",
+      majors: ["mathsmatics"],
+      dates: 2012,
+      url: "http://www.xaut.edu.cn/“
+    }
+    ],
+    onlineCourses: [
+    {
+      title: "JavaScript Crash Course",
+      school: "Udacity",
+      dates: 2016,
+      url: "http://www.udacity.com/course/ud804"
+    }
+    ]
+  };
+
+  var work = {
+    jobs: [
+    {
+      employer: "Tencent",
+      title: "Backend Enginner",
+      dates: 2016,
+      description: "Nothing"
+    }
+    ]
+  };
+
+  var projects = {
+    "projects": [
+      title: "Portfilo",
+      dates: 2017,
+      description: "My portfilo",
+      images: [
+        "https://github.com/blinklv/portfolio/blob/master/readme/desktop.jpg"
+      ]
+    ]
+  };
 })();
 
